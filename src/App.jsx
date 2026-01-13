884fc8131cd57cdd57ca25148cce382e8baa9560
HazelSharmaCoderHZ <sharmahazel310@gmail.com>
2026-01-14 01:43:03 +0530

    StaffAugmentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import StaffAugmentation from "./components/staff-augmentation/StaffAugmentation";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+       
     </>
   );
 }
---
+++ src/components/staff-augmentation/Commitment.jsx
@@ -0,0 +1,14 @@
+const Commitment = () => {
+  return (
+    <section className="max-w-7xl text-left mx-auto px-6 py-20">
+      <h2 className="text-5xl font-extrabold text-black">
+        Committed to
+      </h2>
+      <p className="text-gray-900 text-xl font-bold mt-2">
+        professionalism and excellence
+      </p>
+    </section>
+  );
+};
+
+export default Commitment;
